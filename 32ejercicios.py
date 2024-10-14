ce6889b3af6a481334201eef586ee9ab431a5dce
eduardoalc-ux <al222311490@gmail.com>
2024-10-14 10:29:22 -0600

    Create 32ejercicios.py

---
+++ 32ejercicios.py
@@ -0,0 +1,62 @@
+# variante 1
+# 1: suma de dos numeros 
+a=10
+b=20
+suma = a + b 
+print (f" la suma de {a} y {b} es: {suma}" )
+#----------------------------------------------------
+#2: concatenacion de cadenas 
+cadena1= "hola"
+cadena2= "mundo" 
+resultado = cadena1 + " " + cadena2
+print (resultado)
+#----------------------------------------------------
+#3:calcular el area de un rectangulo 
+base = 5
+altura = 10
+area = base * altura 
+print (f"  el area del rectangulo es : {area}  ")
+##--------------------------------------------------------
+#4:  comprobar si un numero es par o impar 
+num = 7
+if num % 2 = 0:
+resultado = "par"
+else:
+resultado = "impar"
+print (f"el numero {num} es  {resultado}: ")
+##---------------------------------------------------------
+#5: calcular factorial de un numero 
+def factorial (n)
+if n = 0 or n = 1:
+return 1
+else n *
+factorial (n - 1)
+factorial_5 = factorial (5)
+print (f"el factorial de 5 es: {factorial_5} ")
+#--------------------------------------------------------------
+#6: encontar el numero mas grannde de una lista 
+numeros= [1,2,3,4,5]
+numero_grande = max (numeros)
+print ("el numero mas grande es:", numero_grande)
+#--------------------------------------------------------------
+#7: Convertir grados Celsius a Fahrenheit
+celsius = 25
+fahrenheit = celsius * (9/5) + 32
+print (f"{celsius} grados celsius son {fahrenheit} grados fahrenheit. ")
+##---------------------------------------------------------------
+#8: verificar si un numero es primo 
+num= 7
+es_primo = all (numero % i ! = 0 for i  in range (2, int(numero **0.5)+1)) and numero > 1
+print (f"{numero} es un numero primo :"  if es_primo else (f"{numero} no es un numero primo: ")
+#-------------------------------------------------------------------
+#9: cuantas veces aparece un caracter en una cadena 
+cadena "programación"
+caracter "o"
+cantidad =
+cadena.count(caracter)
+print (f"el caracter {caracter} aparece {cantidad} veces en la {cadena}:")
+#
+       
+
+
+
